db4473c5367acaa947d73ebbbe6be5aa98dab476
MuhammadDAFFA28 <daffa.mda28@gmail.com>
2026-05-07 15:14:04 +0700

    feat: add CI pipeline with automated tests

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -64,7 +64,7 @@ def test_delete_task():
 
     result = delete_task(tasks, 1)
 
-    assert len(result) == 99
+    assert len(result) == 1
     assert result[0]["id"] == 2
 
 
@@ -73,5 +73,5 @@ def test_search_task_by_assignee():
 
     result = search_task_by_assignee(tasks, "rina")
 
-    assert len(result) == 99
+    assert len(result) == 1
     assert result[0]["assignee"] == "Rina"
